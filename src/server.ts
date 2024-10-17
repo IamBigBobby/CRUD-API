fb7a952086929217507592845501875bba06d530
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-17 19:11:48 +0400

    feat: add basic requests for server

--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,61 @@
 import * as http from 'http';
 import * as dotenv from 'dotenv';
+import { User } from './interface/user';
+import { createUser } from './utils/createNewUser';
 
 dotenv.config();
 const port = Number(process.env.PORT) || 3000;
 
+const users: User[] = [];
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Server is running\n');
-  });
+  res.setHeader('Content-Type', 'application/json');
+
+  if (req.method === 'GET' && req.url === '/api/users') {
+    console.log('trigger get');
+    res.writeHead(200);
+    res.end(JSON.stringify(users));
+  } else if (req.method === 'POST' && req.url === '/api/users') {
+
+    let body: Buffer[] = [];
+    console.log('trigger post')
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      try {
+        const parsedBody = Buffer.concat(body).toString();
+  
+        if (!parsedBody) {
+          throw new Error('Request body is empty');
+        }
+
+        const { username, age, hobbies } = JSON.parse(parsedBody);
+
+        if (!username || !age || !Array.isArray(hobbies)) {
+          res.writeHead(400);
+          res.end(JSON.stringify({ message: 'Invalid data format' }));
+          return;
+        }
+
+        const newUser = createUser(username, age, hobbies);
+        users.push(newUser);
+
+        res.writeHead(201);
+        res.end(JSON.stringify(newUser));
+      } catch (error) {
+        console.error('Error occurred during POST request:', error);
+        res.writeHead(500);
+        res.end(JSON.stringify({ message: 'Server error' }));
+      }
+    });
+  }
+  })
   
   server.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
 
-// const users: any[] = [];
-
-// const requestListener = (req: http.IncomingMessage, res: http.ServerResponse) => {
-//   res.setHeader('Content-Type', 'application/json');
-
-//   if (req.method === 'GET' && req.url === '/api/users') {
-//     res.writeHead(200);
-//     res.end(JSON.stringify(users));
-//   } else if (req.method === 'POST' && req.url === '/api/users') {
-//     let body: Buffer[] = [];
-
-//     req.on('data', (chunk: Buffer<ArrayBufferLike>) => {
-//       body.push(chunk);
-//     });
-
-//     req.on('end', () => {
-//       const user = JSON.parse(Buffer.concat(body).toString());
-//       users.push(user);
-//       res.writeHead(201);
-//       res.end(JSON.stringify(user));
-//     });
-//   } else {
-//     res.writeHead(404);
-//     res.end(JSON.stringify({ message: 'Not Found' }));
-//   }
-// };
-
-// const server = http.createServer(requestListener);
-
-// server.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 
 
