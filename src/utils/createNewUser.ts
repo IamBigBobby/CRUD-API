17458e422e0698e1fca9d149340a7da75abbf3a9
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-17 18:37:09 +0400

    feat: add creating new user

---
+++ src/utils/createNewUser.ts
@@ -0,0 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../interface/user';
+
+export const createUser = (username: string, age: number, hobbies: string[]): User => {
+    return {
+      id: uuidv4(),
+      username,
+      age,
+      hobbies,
+    };
+};
