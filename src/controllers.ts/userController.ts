29bbb448d5e806b834589d712ab6980a85dd3947
Denis Svetleishii <denissvetleishii@outlook.com>
2024-10-20 15:03:55 +0400

    fix: fix get request by id

--- src/controllers.ts/userController.ts
+++ src/controllers.ts/userController.ts
@@ -1,12 +1,31 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import { User } from '../interfaces/user';
 import { createUser } from '../utils/createNewUser';
+import { v4 as uuidv4, validate as isUuid } from 'uuid';
 
 const users: User[] = [];
 
 export const getAllUsers = (req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(200);
   res.end(JSON.stringify(users));
+};
+
+export const getUserByIdHandler = (req: IncomingMessage, res: ServerResponse, userId: string) => {
+  if (!isUuid(userId)) {
+    res.writeHead(400);
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  const user = users.find(u => u.id === userId);
+  if (!user) {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: 'User not found' }));
+    return;
+  }
+
+  res.writeHead(200);
+  res.end(JSON.stringify(user));
 };
 
 export const createUserHandler = (req: IncomingMessage, res: ServerResponse) => {
@@ -19,14 +38,9 @@ export const createUserHandler = (req: IncomingMessage, res: ServerResponse) => 
   req.on('end', () => {
     try {
       const parsedBody = Buffer.concat(body).toString();
-
-      if (!parsedBody) {
-        throw new Error('Request body is empty');
-      }
-
       const { username, age, hobbies } = JSON.parse(parsedBody);
 
-      if (!username || !age || !Array.isArray(hobbies)) {
+      if (!username || typeof age !== 'number' || !Array.isArray(hobbies)) {
         res.writeHead(400);
         res.end(JSON.stringify({ message: 'Invalid data format' }));
         return;
@@ -38,9 +52,67 @@ export const createUserHandler = (req: IncomingMessage, res: ServerResponse) => 
       res.writeHead(201);
       res.end(JSON.stringify(newUser));
     } catch (error) {
-      console.error('Error occurred during POST request:', error);
       res.writeHead(500);
       res.end(JSON.stringify({ message: 'Server error' }));
     }
   });
+};
+
+export const updateUserHandler = (req: IncomingMessage, res: ServerResponse, userId: string) => {
+  if (!isUuid(userId)) {
+    res.writeHead(400);
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  const userIndex = users.findIndex(u => u.id === userId);
+  if (userIndex === -1) {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: 'User not found' }));
+    return;
+  }
+
+  let body: Buffer[] = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  });
+
+  req.on('end', () => {
+    try {
+      const parsedBody = Buffer.concat(body).toString();
+      const { username, age, hobbies } = JSON.parse(parsedBody);
+
+      if (!username || typeof age !== 'number' || !Array.isArray(hobbies)) {
+        res.writeHead(400);
+        res.end(JSON.stringify({ message: 'Invalid data format' }));
+        return;
+      }
+
+      users[userIndex] = { id: userId, username, age, hobbies };
+      res.writeHead(200);
+      res.end(JSON.stringify(users[userIndex]));
+    } catch (error) {
+      res.writeHead(500);
+      res.end(JSON.stringify({ message: 'Server error' }));
+    }
+  });
+};
+
+export const deleteUserHandler = (req: IncomingMessage, res: ServerResponse, userId: string) => {
+  if (!isUuid(userId)) {
+    res.writeHead(400);
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  const userIndex = users.findIndex(user => user.id === userId);
+  if (userIndex === -1) {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: 'User not found' }));
+    return;
+  }
+
+  users.splice(userIndex, 1);
+  res.writeHead(204);
+  res.end();
 };
--- src/routes.ts/users.ts
+++ src/routes.ts/users.ts
@@ -1,13 +1,19 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { createUserHandler, getAllUsers } from '../controllers.ts/userController';
+import { createUserHandler, deleteUserHandler, getAllUsers, getUserByIdHandler, updateUserHandler } from '../controllers.ts/userController';
+
 
 export const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
   res.setHeader('Content-Type', 'application/json');
+
+  const urlParts = req.url ? req.url.split('/') : [];
 
   switch (req.method) {
     case 'GET':
       if (req.url === '/api/users') {
         getAllUsers(req, res);
+      } else if (urlParts.length === 4 && urlParts[1] === 'api' && urlParts[2] === 'users' && urlParts[3]) {
+        console.log('trigger');
+        getUserByIdHandler(req, res, urlParts[3]);
       } else {
         res.writeHead(404);
         res.end(JSON.stringify({ message: 'Not Found' }));
@@ -23,10 +29,27 @@ export const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
       }
       break;
 
+    case 'PUT':
+      if (urlParts.length === 4 && urlParts[1] === 'api' && urlParts[2] === 'users' && urlParts[3]) {
+        updateUserHandler(req, res, urlParts[3]);
+      } else {
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: 'Not Found' }));
+      }
+      break;
+
+    case 'DELETE':
+      if (urlParts.length === 4 && urlParts[1] === 'api' && urlParts[2] === 'users' && urlParts[3]) {
+        deleteUserHandler(req, res, urlParts[3]);
+      } else {
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: 'Not Found' }));
+      }
+      break;
+
     default:
       res.writeHead(404);
       res.end(JSON.stringify({ message: 'Not Found' }));
       break;
   }
 };
-
